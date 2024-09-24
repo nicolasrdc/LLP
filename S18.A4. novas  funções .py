790b88cd3708299c1b5c66416e4171408298a481
NicolasCosta <nicolas.costa41@hotmail.com>
2024-09-24 11:15:32 -0300

    Add files via upload

---
+++ S18.A4. novas  funções .py
@@ -0,0 +1,9 @@
+# A função map converte a STRING para um número INTEIRO ou um número REAL
+
+valores = map(float, input().split())
+
+# list é a função de fazer lista
+
+valores_lista = list(valores)
+print(valores_lista) 
+ 
